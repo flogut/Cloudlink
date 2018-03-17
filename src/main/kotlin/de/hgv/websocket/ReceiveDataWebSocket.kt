edfd921e29599db226dd21bf424ee66b92dc6d53
flogut <florian.gutekunst@gmx.de>
2018-03-17 12:23:27 +0100

    Removed authentication from reading WebSockets

--- src/main/kotlin/de/hgv/websocket/ReceiveDataWebSocket.kt
+++ src/main/kotlin/de/hgv/websocket/ReceiveDataWebSocket.kt
@@ -5,7 +5,6 @@ import com.github.salomonbrys.kodein.conf.global
 import com.github.salomonbrys.kodein.instance
 import io.javalin.embeddedserver.jetty.websocket.WsSession
 import org.eclipse.jetty.websocket.api.Session
-import org.eclipse.jetty.websocket.api.StatusCode
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect
 import org.eclipse.jetty.websocket.api.annotations.WebSocket
 
@@ -13,20 +12,11 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket
 class ReceiveDataWebSocket {
 
     private val receiveDataSessions: MutableList<WsSession> = Kodein.global.instance("receiveDataSessions")
-    private val receiveTokens: List<String> = Kodein.global.instance("receiveTokens")
 
     @OnWebSocketConnect
     fun onConnect(session: Session) {
         val wsSession = WsSession(session)
-        if (wsSession.queryString() == null || wsSession.anyQueryParamNull("token") || !receiveTokens.contains(
-                wsSession.queryParam("token")
-            )
-        ) {
-            wsSession.close(StatusCode.POLICY_VIOLATION, "Unauthorized")
-            wsSession.disconnect()
-        } else {
-            receiveDataSessions.add(wsSession)
-        }
+        receiveDataSessions.add(wsSession)
     }
 
 }
